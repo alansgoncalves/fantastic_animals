146995abc3e1a82d9c3bcee63b626fe8fb54ea21
alansgoncalves <vekio.soft@gmail.com>
2022-04-05 11:10:15 -0300

    fix html tag

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-const animais = document.getElementsByTagName('ul')
-console.log(animais[1])
+// const animais = document.querySelector('.grid-section')
+// console.log(animais)
+
+const htmlCollection = document.getElementsByClassName('grid-section');
+const nodeList = document.querySelectorAll('.grid-section');
+
+console.log(htmlCollection)
+console.log(nodeList)
